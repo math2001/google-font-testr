9b04e1f2960b02f56722f41b602431dbe4ec2241
Mathieu PATUREL <australie.p@gmail.com>
2017-04-11 09:42:26 +1000

    :art: popup working when clicking 1+ times on icon

--- trigger.js
+++ trigger.js
@@ -1,5 +1,6 @@
 "use strict";
 
 chrome.browserAction.onClicked.addListener(tab => {
-    chrome.tabs.executeScript(null, {file: 'app.js'})
+    chrome.tabs.executeScript(null, {file: 'insert.js'})
+    chrome.tabs.insertCSS(null, {file: 'popup.css'})
 })
