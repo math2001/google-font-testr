8f662482cdbcee49a88caee7b4a4d75e6c859376
Mathieu PATUREL <australie.p@gmail.com>
2017-04-11 14:59:38 +1000

    :art: :fire: remove trigger.js (unused)

--- trigger.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-chrome.browserAction.onClicked.addListener(tab => {
-    chrome.tabs.executeScript(null, {file: 'insert.js'})
-    // chrome.tabs.insertCSS(null, {file: 'popup.css'})
-})
