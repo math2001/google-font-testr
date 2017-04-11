c0e6bf9490edb37c8055a41729b0f5dee3cf9ab4
Mathieu PATUREL <australie.p@gmail.com>
2017-04-11 11:28:31 +1000

    :sparkles: reuse the same tags when re-showing popup

--- listener.js
+++ listener.js
@@ -2,6 +2,9 @@
 
 if (window.FontManager === undefined) {
 
+const FONT_LOADER_ID = 'google-font-testr-font-loader'
+const STYLE_SHEET_ID = 'google-font-testr-stylesheet'
+
 chrome.runtime.onMessage.addListener(fontInfos => {
 
     if (fontInfos === 'reset') {
@@ -9,32 +12,47 @@ chrome.runtime.onMessage.addListener(fontInfos => {
     } else {
         window.fontManager.use(fontInfos.selector, ...fontInfos.fonts)
     }
-
 })
-
 
 class FontManager {
 
     constructor() {
-        this.fontLoaderTag = this.createFontLoader()
-        this.stylesheet = this.createStylesheet()
+        this.fontLoaderTag = this.getFontLoader()
+        this.stylesheet = this.getStylesheet()
+    }
+
+    getStylesheet() {
+        return document.head.querySelector('#' + STYLE_SHEET_ID) || this.createStylesheet()
     }
 
     createStylesheet() {
         const stylesheet = document.createElement('style')
         stylesheet.type = 'text/css'
+        stylesheet.id = STYLE_SHEET_ID
         return document.head.appendChild(stylesheet)
+    }
+
+    getFontLoader() {
+        const fontLoader = document.getElementById(FONT_LOADER_ID)
+        if (fontLoader === null) {
+            return this.createFontLoader()
+        }
+        return fontLoader
     }
 
     createFontLoader() {
         // create a <link> tag which is used to load the font
         const fontLoader = document.createElement('link')
         fontLoader.rel = 'stylesheet'
+        fontLoader.id = FONT_LOADER_ID
         document.head.appendChild(fontLoader)
         return fontLoader
     }
 
-    _load(...fontFamilies) {
+    _load(fontFamilies) {
+        if (typeof fontFamilies === 'string') {
+            fontFamilies = [fontFamilies]
+        }
         fontFamilies = fontFamilies.join('|').replace(/ /g, '+')
         this.fontLoaderTag.href = `https://fonts.googleapis.com/css?family=${fontFamilies}`
     }
@@ -51,6 +69,7 @@ class FontManager {
         this.fontLoaderTag.href = ''
     }
 }
+
 
 window.fontManager = new FontManager()
 
